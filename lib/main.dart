2e85f89206ec0030f547095ea4ee70b6fdf32032
JannatulFerdousRiva <riva15-4928@diu.edu.bd>
2025-04-14 12:52:07 +0600

    task_2

--- lib/main.dart
+++ lib/main.dart
@@ -6,20 +6,56 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text(
-            "Hello world!",
-            style: TextStyle(
-              fontSize: 50,
-              fontFamily: 'Poppins',
-              fontWeight: FontWeight.bold,
-              color: const Color.fromARGB(255, 255, 7, 255),
+        appBar: AppBar(
+          title: Text("Task_02"),
+          centerTitle: true,
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: ButtonPress(),
+      ),
+    );
+  }
+}
+
+class ButtonPress extends StatefulWidget {
+  const ButtonPress({super.key});
+
+  @override
+  State<ButtonPress> createState() => _ButtonPressState();
+}
+
+class _ButtonPressState extends State<ButtonPress> {
+  String _displyText = "Click The Button";
+
+  void afterButtonPress() {
+    setState(() {
+      _displyText = "Button Pressed";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              _displyText,
+              style: TextStyle(fontSize: 25, color: Colors.redAccent),
             ),
-          ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: afterButtonPress,
+              child: Text("Click Here"),
+            ),
+          ],
         ),
       ),
     );
